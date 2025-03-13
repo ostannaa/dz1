a099d0ee4cfd5e38ae8235dc93c6027cdc258237
Anastasiia Ostrovska <anastasiiaostrovska@MacBook-Air-Anastasiia.local>
2025-03-13 21:23:09 +0200

    Оновлення файлів

---
+++ dz3.py
@@ -0,0 +1,34 @@
+parol = "111"
+if "password123" not in parol: print('Wrong password')
+else : print( "You entered the system")
+
+day = 10
+if day == 1 : print('Monday')
+elif day == 2 : print('Tuesday')
+elif day == 3 : print('Wednesday')
+elif day == 4 : print('Thursday')
+elif day == 5 : print('Friday')
+elif day == 6 : print('Saturday')
+elif day == 7 : print('Sunday')
+else : print('Wrong date')
+
+num_1 = 4
+for i in range(1 , 10):
+    p = (num_1 * i)
+    print(p)
+
+list = [1, 3 , 5 ,7]
+summa = sum(list)
+print(summa)
+
+num_3 = 5
+for i in range(1 , num_3 + 1):
+    num_3 *= i
+    print(f"fact {num_3} дорівнює {num_3}")
+
+
+
+
+
+
+
